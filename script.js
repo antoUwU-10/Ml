bc0c93eab86bb776779d647dde9995c6586cd155
antoUwU-10 <anto.uwu1008@gmail.com>
2024-12-18 22:00:27 -0400

    Deploy ML

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-const myDate = new Date(); 
-const xmas = Date.parse("Dec 25, "+myDate.getFullYear()) 
-const today = Date.parse(myDate) 
+// Función para obtener la fecha y hora ajustada a Bolivia (UTC-4)
+function getBoliviaTime() {
+    const now = new Date();
+    const utcTime = now.getTime() + now.getTimezoneOffset() * 60000; // Convertimos a UTC
+    return new Date(utcTime - 4 * 60 * 60000); // Ajustamos a UTC-4
+}
 
-const daysToChristmas = Math.round((xmas-today)/(1000*60*60*24)) 
+const myDate = getBoliviaTime();
+const xmas = Date.parse(`Dec 25, ${myDate.getFullYear()} 00:00:00 GMT-0400`);
+const today = myDate.getTime();
 
+const daysToChristmas = Math.round((xmas - today) / (1000 * 60 * 60 * 24));
 
-if (daysToChristmas == 0) 
-$('#days').text("It's Christmas!! Merry Christmas!");
+if (daysToChristmas === 0)
+    $('#days').text("It's Christmas!! Merry Christmas!");
 
-if (daysToChristmas < 0) 
-$('#days').text("Christmas was "+-1*(daysToChristmas)+" days ago.");
+if (daysToChristmas < 0)
+    $('#days').text(`Christmas was ${-1 * daysToChristmas} days ago.`);
 
-if (daysToChristmas > 0) 
-$('#days').text(daysToChristmas+" days to Christmas!");
+if (daysToChristmas > 0)
+    $('#days').text(`${daysToChristmas} days to Christmas!`);
 
-//make snow
+// Make snow
 snowDrop(150, randomInt(1035, 1280));
 snow(150, 150);
 
 function snow(num, speed) {
-		if (num > 0) {
-			setTimeout(function () {
-				$('#drop_' + randomInt(1, 250)).addClass('animate');
-				num--;
-				snow(num, speed);
-			}, speed);
-		}
-	};
+    if (num > 0) {
+        setTimeout(function () {
+            $('#drop_' + randomInt(1, 250)).addClass('animate');
+            num--;
+            snow(num, speed);
+        }, speed);
+    }
+}
 
-	function snowDrop(num, position) {
-		if (num > 0) {
-			var drop = '<div class="drop snow" id="drop_' + num + '"></div>';
+function snowDrop(num, position) {
+    if (num > 0) {
+        var drop = `<div class="drop snow" id="drop_${num}"></div>`;
 
-			$('body').append(drop);
-			$('#drop_' + num).css('left', position);
-			num--;
-			snowDrop(num, randomInt(60, 1280));
-		}
-	};
+        $('body').append(drop);
+        $(`#drop_${num}`).css('left', position);
+        num--;
+        snowDrop(num, randomInt(60, 1280));
+    }
+}
 
 function randomInt(min, max) {
-		return Math.floor(Math.random() * (max - min + 1) + min);
-	};
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
