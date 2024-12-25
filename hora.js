3abc100b271e2e59c0243c994cce3ec7990ea967
antoUwU-10 <anto.uwu1008@gmail.com>
2024-12-24 21:25:21 -0400

    Deploy

--- hora.js
+++ hora.js
@@ -1,3 +1,3 @@
 // hora.js
-export const targetDate1 = new Date("2024-12-24T21:35:00-04:00"); // 5:13 AM del 22 de diciembre de 2024
-export const targetDate2 = new Date("2024-12-24T21:35:01-04:00"); // 5:13:01 AM del 22 de diciembre de 2024
+export const targetDate1 = new Date("2024-12-24T21:27:00-04:00"); // 5:13 AM del 22 de diciembre de 2024
+export const targetDate2 = new Date("2024-12-24T21:27:01-04:00"); // 5:13:01 AM del 22 de diciembre de 2024
